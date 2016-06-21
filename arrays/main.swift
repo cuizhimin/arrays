60bb19cb2e3287e57e47a5b4c6eac31340d17724
cuizhimin <inocent@foxmail.com>
2016-06-21 11:26:05 +0800

    arrays
    首先用var定义三个变量并赋值，再定义一个数组并赋值，然后是for循环，并且在其中继续镶嵌一个for循环，使i循环17遍，j再将数遍历一边，没有
    遗漏。在循环中，如果i的值小于j的值，则把i的值赋给temp，把j的值赋给i，把temp的值赋给j。最后利用value使数据一行输出一个。

--- arrays/main.swift
+++ arrays/main.swift
@@ -6,7 +6,24 @@
 //  Copyright © 2016年 czm. All rights reserved.
 //
 
+
 import Foundation
 
-print("Hello, World!")
-
+var i=1,j=0,temp=0
+var
+arr=[0,3,5,7,9,10,1,4,2,6,8,11,15,13,12,14,18,10]
+for i in 0...17
+{
+    for j in 0...17
+    {
+        if(arr[i]<arr[j]){
+            temp=arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+        }
+    }
+}
+for value in arr
+{
+    print(value)
+}
